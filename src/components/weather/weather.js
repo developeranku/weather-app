1bc9efaad2e25db7c976e0d7b38c72003401d6ea
developeranku <ankur4736@gmail.com>
2019-08-24 14:47:21 +0530

    ankur4736@gmail.com

---
+++ src/components/weather/weather.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Weather extends React.Component {
+    render() {
+        return <h1>Welcome to Weather App</h1>
+    }
+}
+
+export default Weather;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './components/weather/weather';
 import * as serviceWorker from './serviceWorker';
+import Weather from './components/weather/weather';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Weather />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
