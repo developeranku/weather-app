55ca27ce07645bf40c0a1ceab604cacdae26a881
developeranku <ankur4736@gmail.com>
2019-08-26 13:03:21 +0530

    done

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -1,20 +1,10 @@
 import React from 'react';
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?q=Gurgaon&APPID=77c7ae9c2ac84dff19c359c057fbf19b';
+const API_KEY = '77c7ae9c2ac84dff19c359c057fbf19b';
 
-const weatherData = []
-
+const url = `http://api.openweathermap.org/data/2.5/forecast?q=Gurgaon&APPID=${API_KEY}`;
+console.log(url);
 class Weather extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { data: '' };
-    }
-
-    componentDidMount() {
-        fetch(url).then(res => res.json()).then(data => weatherData.push(data));
-        console.log(weatherData);
-    }
-
     render() {
         return <h1>Welcome to Weather App</h1>
     }
