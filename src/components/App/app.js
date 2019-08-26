7dc9485213a1d6f36a858ee6b22d658783a709a0
developeranku <ankur4736@gmail.com>
2019-08-26 19:19:38 +0530

    update

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Weather } from './../Weather/weather';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <Weather />
             </React.Fragment>
         )
     }
--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Form } from './../Form/form';
 
 const API_KEY = '77c7ae9c2ac84dff19c359c057fbf19b';
 const url = `http://api.openweathermap.org/data/2.5/forecast?q=Gurgaon&APPID=${API_KEY}`;
-console.log(url);
 
-class Weather extends React.Component {
+export class Weather extends React.Component {
+
+    componentDidMount() {
+        fetch(url).then(res => res.json()).then(data => console.log(data))
+    }
+
     render() {
-        return <h1>Welcome to Weather App</h1>
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <Form />
+                    </div>
+                    <div className="col-md-8">
+                        <h1>Welcome to Weather App</h1>
+                    </div>
+                </div>
+            </div>
+        )
+
     }
 }
 
-export default Weather;
