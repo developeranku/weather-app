599d6225989ee589d7ab89e56294b63b58717fd1
developeranku <ankur4736@gmail.com>
2019-08-29 13:07:38 +0530

    update

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -7,23 +7,23 @@ export class Weather extends React.Component {
     render() {
         return (
             <React.Fragment>
-
-                <div className="row weatherData text-center">
+                <div className="row weatherData">
 
                     <div className="col-md-4 humidity">
                         <img src={humidityIcon} alt='icon' />
-                        <p>Humidity: <br /> {this.props.humidity}</p>
+                        <p>Humidity: <br /> {this.props.humidity} %</p>
                         <p>{this.props.city}</p>
                     </div>
 
                     <div className="col-md-4 pressure">
                         <img src={pressureIcon} alt='icon' />
-                        <p>Pressure: <br /> {this.props.pressure}</p>
+                        <p>Pressure: <br /> {this.props.pressure} %</p>
                     </div>
 
                     <div className="col-md-4 temperature">
                         <img src={temperatureIcon} alt='icon' />
-                        <p>Temperature: <br /> {this.props.temp}</p>
+                        <p>Temperature: <br /> {this.props.temp} &#176;
+                        C</p>
                     </div>
 
                 </div>
